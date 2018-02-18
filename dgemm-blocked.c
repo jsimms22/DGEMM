f94056fcbf67bd2beba6b430ddbdfee2c092c109
jsimms22 <36178008+jsimms22@users.noreply.github.com>
2018-02-18 17:40:20 -0600

    working avx - needs changes

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -22,17 +22,155 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #define min(a,b) (((a)<(b))?(a):(b))
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
 
+#define ARRAY(A,i,j) (A)[(j)*lda + (i)]
+
+static inline void calc_4x4(int lda, int K, double* a, double* b, double* c)
+{
+  __m128d a0x_1x, a2x_3x, 
+    bx0, bx1, bx2, bx3, 
+    c00_10, c20_30, 
+    c01_11, c21_31,
+    c02_12, c22_32,
+    c03_13, c23_33;
+
+  double* c01_11_ptr = c + lda;
+  double* c02_12_ptr = c01_11_ptr + lda;
+  double* c03_13_ptr = c02_12_ptr + lda;
+
+  c00_10 = _mm_loadu_pd(c);
+  c20_30 = _mm_loadu_pd(c+2);
+  c01_11 = _mm_loadu_pd(c01_11_ptr);
+  c21_31 = _mm_loadu_pd(c01_11_ptr+2);
+  c02_12 = _mm_loadu_pd(c02_12_ptr);
+  c22_32 = _mm_loadu_pd(c02_12_ptr+2);
+  c03_13 = _mm_loadu_pd(c03_13_ptr);
+  c23_33 = _mm_loadu_pd(c03_13_ptr+2);
+
+  for (int x = 0; x < K; ++x) 
+  {
+    a0x_1x = _mm_load_pd(a);
+    a2x_3x = _mm_load_pd(a+2);
+    a += 4;
+
+    bx0 = _mm_loaddup_pd(b++);
+    bx1 = _mm_loaddup_pd(b++);
+    bx2 = _mm_loaddup_pd(b++);
+    bx3 = _mm_loaddup_pd(b++);
+
+    c00_10 = _mm_add_pd(c00_10, _mm_mul_pd(a0x_1x, bx0));
+    c20_30 = _mm_add_pd(c20_30, _mm_mul_pd(a2x_3x, bx0));
+    c01_11 = _mm_add_pd(c01_11, _mm_mul_pd(a0x_1x, bx1));
+    c21_31 = _mm_add_pd(c21_31, _mm_mul_pd(a2x_3x, bx1));
+    c02_12 = _mm_add_pd(c02_12, _mm_mul_pd(a0x_1x, bx2));
+    c22_32 = _mm_add_pd(c22_32, _mm_mul_pd(a2x_3x, bx2));
+    c03_13 = _mm_add_pd(c03_13, _mm_mul_pd(a0x_1x, bx3));
+    c23_33 = _mm_add_pd(c23_33, _mm_mul_pd(a2x_3x, bx3));
+  }
+
+  _mm_storeu_pd(c, c00_10);
+  _mm_storeu_pd((c+2), c20_30);
+  _mm_storeu_pd(c01_11_ptr, c01_11);
+  _mm_storeu_pd((c01_11_ptr+2), c21_31);
+  _mm_storeu_pd(c02_12_ptr, c02_12);
+  _mm_storeu_pd((c02_12_ptr+2), c22_32);
+  _mm_storeu_pd(c03_13_ptr, c03_13);
+  _mm_storeu_pd((c03_13_ptr+2), c23_33);
+}
+
+static inline void copy_a (int lda, const int K, double* a_src, double* a_dest) {
+  /* For each 4xK block-row of A */
+  for (int i = 0; i < K; ++i) 
+  {
+    *a_dest++ = *a_src;
+    *a_dest++ = *(a_src+1);
+    *a_dest++ = *(a_src+2);
+    *a_dest++ = *(a_src+3);
+    a_src += lda;
+  }
+}
+
+static inline void copy_b (int lda, const int K, double* b_src, double* b_dest) {
+  double *b_ptr0, *b_ptr1, *b_ptr2, *b_ptr3;
+  b_ptr0 = b_src;
+  b_ptr1 = b_ptr0 + lda;
+  b_ptr2 = b_ptr1 + lda;
+  b_ptr3 = b_ptr2 + lda;
+
+  for (int i = 0; i < K; ++i) 
+  {
+    *b_dest++ = *b_ptr0++;
+    *b_dest++ = *b_ptr1++;
+    *b_dest++ = *b_ptr2++;
+    *b_dest++ = *b_ptr3++;
+  }
+}
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+static inline void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
+  double A_block[M*K], B_block[K*N];
+  double *a_ptr, *b_ptr, *c;
 
- // int edge1 = M % 4; int edge2 = M % 4;
+  const int Nmax = N-3;
+  int Mmax = M-3;
+  int fringe1 = M%4;
+  int fringe2 = N%4;
+
+  int i = 0, j = 0, p = 0;
+
+  /* For each column of B */
+  for (j = 0 ; j < Nmax; j += 4) 
+  {
+    b_ptr = &B_block[j*K];
+    // copy and transpose B_block
+    copy_b(lda, K, B + j*lda, b_ptr);
+    /* For each row of A */
+    for (i = 0; i < Mmax; i += 4) {
+      a_ptr = &A_block[i*K];
+      if (j == 0) copy_a(lda, K, A + i, a_ptr);
+      c = C + i + j*lda;
+      calc_4x4(lda, K, a_ptr, b_ptr, c);
+    }
+  }
+
+  /* Handle "fringes" */
+  if (fringe1 != 0) 
+  {
+    /* For each row of A */
+    for ( ; i < M; ++i)
+      /* For each column of B */ 
+      for (p = 0; p < N; ++p) 
+      {
+        /* Compute C[i,j] */
+        double c_ip = ARRAY(C,i,p);
+        for (int k = 0; k < K; ++k)
+          c_ip += ARRAY(A,i,k) * ARRAY(B,k,p);
+        ARRAY(C,i,p) = c_ip;
+      }
+  }
+  if (fringe2 != 0) 
+  {
+    Mmax = M - fringe1;
+    /* For each column of B */
+    for ( ; j < N; ++j)
+      /* For each row of A */ 
+      for (i = 0; i < Mmax; ++i) 
+      {
+        /* Compute C[i,j] */
+        double cij = ARRAY(C,i,j);
+        for (int k = 0; k < K; ++k)
+          cij += ARRAY(A,i,k) * ARRAY(B,k,j);
+        ARRAY(C,i,j) = cij;
+      }
+  }
+   // int edge1 = M % 4; int edge2 = M % 4;
   //Do math here
   //if (K % 4 == 0) {
-  __m256d m0,m1,m2,m3;
+  /*__m256d m0,m1,m2,m3;
   for (int i = 0; i < M; i += 4) {
     for (int j = 0; j < N; ++j) {
       m0 = _mm256_setzero_pd();  
@@ -48,7 +186,7 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
     }
   }
   //}
-
+  */
   /*double m0,m1,m2,m3 = 0.0;
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
