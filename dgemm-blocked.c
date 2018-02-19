6b0e9d4ee4e5ad4b86c5a800a339795db9cb00e0
Jeremiah Simmons <jpsimms2057@gmail.com>
2018-02-18 19:39:45 -0500

    updated

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -29,7 +29,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 
 static inline void calc_4x4(int lda, int K, double* a, double* b, double* c)
 {
-  __m128d a0x_1x, a2x_3x, 
+  __m256d a0x_1x, a2x_3x, 
     bx0, bx1, bx2, bx3, 
     c00_10, c20_30, 
     c01_11, c21_31,
@@ -40,44 +40,44 @@ static inline void calc_4x4(int lda, int K, double* a, double* b, double* c)
   double* c02_12_ptr = c01_11_ptr + lda;
   double* c03_13_ptr = c02_12_ptr + lda;
 
-  c00_10 = _mm_loadu_pd(c);
-  c20_30 = _mm_loadu_pd(c+2);
-  c01_11 = _mm_loadu_pd(c01_11_ptr);
-  c21_31 = _mm_loadu_pd(c01_11_ptr+2);
-  c02_12 = _mm_loadu_pd(c02_12_ptr);
-  c22_32 = _mm_loadu_pd(c02_12_ptr+2);
-  c03_13 = _mm_loadu_pd(c03_13_ptr);
-  c23_33 = _mm_loadu_pd(c03_13_ptr+2);
+  c00_10 = _mm256_load_pd(c);
+  c20_30 = _mm256_load_pd(c+4);
+  c01_11 = _mm256_load_pd(c01_11_ptr);
+  c21_31 = _mm256_load_pd(c01_11_ptr + 4);
+  c02_12 = _mm256_load_pd(c02_12_ptr);
+  c22_32 = _mm256_load_pd(c02_12_ptr + 4);
+  c03_13 = _mm256_load_pd(c03_13_ptr);
+  c23_33 = _mm256_load_pd(c03_13_ptr + 4);
 
   for (int x = 0; x < K; ++x) 
   {
-    a0x_1x = _mm_load_pd(a);
-    a2x_3x = _mm_load_pd(a+2);
-    a += 4;
+    a0x_1x = _mm256_load_pd(a);
+    a2x_3x = _mm256_load_pd(a+4);
+    a += 8;
 
-    bx0 = _mm_loaddup_pd(b++);
-    bx1 = _mm_loaddup_pd(b++);
-    bx2 = _mm_loaddup_pd(b++);
-    bx3 = _mm_loaddup_pd(b++);
+    bx0 = _mm256_load_pd(b++);
+    bx1 = _mm256_load_pd(b++);
+    bx2 = _mm256_load_pd(b++);
+    bx3 = _mm256_load_pd(b++);
 
-    c00_10 = _mm_add_pd(c00_10, _mm_mul_pd(a0x_1x, bx0));
-    c20_30 = _mm_add_pd(c20_30, _mm_mul_pd(a2x_3x, bx0));
-    c01_11 = _mm_add_pd(c01_11, _mm_mul_pd(a0x_1x, bx1));
-    c21_31 = _mm_add_pd(c21_31, _mm_mul_pd(a2x_3x, bx1));
-    c02_12 = _mm_add_pd(c02_12, _mm_mul_pd(a0x_1x, bx2));
-    c22_32 = _mm_add_pd(c22_32, _mm_mul_pd(a2x_3x, bx2));
-    c03_13 = _mm_add_pd(c03_13, _mm_mul_pd(a0x_1x, bx3));
-    c23_33 = _mm_add_pd(c23_33, _mm_mul_pd(a2x_3x, bx3));
+    c00_10 = _mm256_add_pd(c00_10, _mm256_mul_pd(a0x_1x, bx0));
+    c20_30 = _mm256_add_pd(c20_30, _mm256_mul_pd(a2x_3x, bx0));
+    c01_11 = _mm256_add_pd(c01_11, _mm256_mul_pd(a0x_1x, bx1));
+    c21_31 = _mm256_add_pd(c21_31, _mm256_mul_pd(a2x_3x, bx1));
+    c02_12 = _mm256_add_pd(c02_12, _mm256_mul_pd(a0x_1x, bx2));
+    c22_32 = _mm256_add_pd(c22_32, _mm256_mul_pd(a2x_3x, bx2));
+    c03_13 = _mm256_add_pd(c03_13, _mm256_mul_pd(a0x_1x, bx3));
+    c23_33 = _mm256_add_pd(c23_33, _mm256_mul_pd(a2x_3x, bx3));
   }
 
-  _mm_storeu_pd(c, c00_10);
-  _mm_storeu_pd((c+2), c20_30);
-  _mm_storeu_pd(c01_11_ptr, c01_11);
-  _mm_storeu_pd((c01_11_ptr+2), c21_31);
-  _mm_storeu_pd(c02_12_ptr, c02_12);
-  _mm_storeu_pd((c02_12_ptr+2), c22_32);
-  _mm_storeu_pd(c03_13_ptr, c03_13);
-  _mm_storeu_pd((c03_13_ptr+2), c23_33);
+  _mm256_storeu_pd(c, c00_10);
+  _mm256_storeu_pd((c+4), c20_30);
+  _mm256_storeu_pd(c01_11_ptr, c01_11);
+  _mm256_storeu_pd((c01_11_ptr+4), c21_31);
+  _mm256_storeu_pd(c02_12_ptr, c02_12);
+  _mm256_storeu_pd((c02_12_ptr+4), c22_32);
+  _mm256_storeu_pd(c03_13_ptr, c03_13);
+  _mm256_storeu_pd((c03_13_ptr+4), c23_33);
 }
 
 static inline void copy_a (int lda, const int K, double* a_src, double* a_dest) {
@@ -115,21 +115,21 @@ static inline void do_block (int lda, int M, int N, int K, double* A, double* B,
   double A_block[M*K], B_block[K*N];
   double *a_ptr, *b_ptr, *c;
 
-  const int Nmax = N-3;
-  int Mmax = M-3;
-  int fringe1 = M%4;
-  int fringe2 = N%4;
+  const int Nmax = N-7;
+  int Mmax = M-7;
+  int fringe1 = M%8;
+  int fringe2 = N%8;
 
   int i = 0, j = 0, p = 0;
 
   /* For each column of B */
-  for (j = 0 ; j < Nmax; j += 4) 
+  for (j = 0 ; j < Nmax; j += 8) 
   {
     b_ptr = &B_block[j*K];
     // copy and transpose B_block
     copy_b(lda, K, B + j*lda, b_ptr);
     /* For each row of A */
-    for (i = 0; i < Mmax; i += 4) {
+    for (i = 0; i < Mmax; i += 8) {
       a_ptr = &A_block[i*K];
       if (j == 0) copy_a(lda, K, A + i, a_ptr);
       c = C + i + j*lda;
@@ -251,7 +251,7 @@ void square_dgemm (/*int iii,*/int lda, double* A, double* B, double* C)
 
   //Block size in 1D for L2 cache - BLOCK2 - for L1 cache - BLOCK 1 - 
   int BLOCK1 = 64;
-  //int BLOCK2 = 128;
+  int BLOCK2 = 128;
   
   //This is for testing dgemm aglorithm outside do_block function
   /*__m128d m0,m1,m2,m3;
@@ -269,7 +269,7 @@ void square_dgemm (/*int iii,*/int lda, double* A, double* B, double* C)
   }*/
   
   //Proposed blocking method for 2 levels of memory - L1 and L2
-  /*for (int x = 0; x < lda; x += BLOCK2) {
+  for (int x = 0; x < lda; x += BLOCK2) {
     int lim_k = x + min (BLOCK2,lda-x);
     for (int y = 0; y < lda; y += BLOCK2) {
       int lim_j = y + min (BLOCK2,lda-y);
@@ -290,10 +290,10 @@ void square_dgemm (/*int iii,*/int lda, double* A, double* B, double* C)
         }
       }
     }
-  }*/
+  }
  
   //Proposed blocking method for 1 level of memory - L1
-  for (int i = 0; i < lda; i += BLOCK1) {
+  /*for (int i = 0; i < lda; i += BLOCK1) {
     int M = min (BLOCK1,lda-i);
     for (int j = 0; j < lda; j += BLOCK1) {
       int N = min (BLOCK1,lda-j);
@@ -305,7 +305,7 @@ void square_dgemm (/*int iii,*/int lda, double* A, double* B, double* C)
 	do_block (lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
       }
     }
-  }
+  }*/
 
   //This is to print out a small n*n C matrix (after calculation)
   /*if (lda == 96) {
