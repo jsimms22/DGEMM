8f4cebb95b63e2ce36987626301eb56bf4f7f714
Jeremiah Simmons <jpsimms2057@gmail.com>
2018-02-19 21:58:53 -0500

    updated with valgrind

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -22,23 +22,10 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #define min(a,b) (((a)<(b))?(a):(b))
-//#define likely(x)       __builtin_expect((x),1)
-//#define unlikely(x)     __builtin_expect((x),0)
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
 
-//#define ARRAY(A,i,j) (A)[(j)*lda + (i)]
-/* This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static inline void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
-{
-  printf("M = %d\t,N = %d,K = %d, \n",M,N,K);
-  //Do math here
-  //printf("Did it declare?");
-  int fringe1 = M % 4;
-  int fringe2 = N % 4;
-  int fringe3 = K % 4;
-  //printf("Did it declare?");
-  if (fringe1 == 0 && fringe2 == 0 && fringe3 == 0 && K == M && M == N && N == K) {
+static inline void do_avx (int lda, int M, int N, int K, double* a, double* b, double* c) {
     //printf("Did it declare?");
     __m256d m0,m1,m2,m3;
     //printf("Did it declare?");
@@ -46,17 +33,19 @@ static inline void do_block (int lda, int M, int N, int K, double* A, double* B,
       for (int j = 0; j < N; ++j) {
         m0 = _mm256_setzero_pd();  
         for (int k = 0; k < K; ++k) {
-	  m1 = _mm256_load_pd(A+i+k*lda);
-	  m2 = _mm256_broadcast_sd(B+k+j*lda);
+	  m1 = _mm256_load_pd(a+i+k*lda);
+	  m2 = _mm256_broadcast_sd(b+k+j*lda);
 	  m3 = _mm256_mul_pd(m1,m2);
 	  m0 = _mm256_add_pd(m0,m3);
         }
-        m1 = _mm256_load_pd(C+i+j*lda);
+        m1 = _mm256_load_pd(c+i+j*lda);
         m0 = _mm256_add_pd(m0,m1);
-        _mm256_store_pd(C+i+j*lda,m0);
+        _mm256_store_pd(c+i+j*lda,m0);
       }
     }
-  } else {
+}
+
+static inline void do_simple (int lda, int M, int N, int K, double* a, double* b, double* c) {
     //printf("Did it do else?");
     // For each row of A
     for (int i = 0 ; i < M; ++i) {
@@ -65,37 +54,40 @@ static inline void do_block (int lda, int M, int N, int K, double* A, double* B,
         // Compute C[i,j] 
         double cij = 0.0;
         for (int k = 0; k < K; ++k){
-          cij += A[i+k*lda] * B[k+j*lda];
+          cij += a[i+k*lda] * b[k+j*lda];
 	}
-        C[i+j*lda] += cij;
+        c[i+j*lda] += cij;
       }
     }
-  }
-  printf("exiting a block\n"); 
 }
-
+/* This auxiliary subroutine performs a smaller dgemm operation
+ *  C := C + A * B
+ * where C is M-by-N, A is M-by-K, and B is K-by-N. */
+static inline void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+{
+  //printf("M = %d\t,N = %d,K = %d, \n",M,N,K);
+  //printf("Did it declare?");
+  int fringe1 = M % 4;
+  int fringe2 = N % 4;
+  int fringe3 = K % 4;
+  //printf("Did it declare?");
+  if (fringe1 == 0 && fringe2 == 0 && fringe3 == 0 && K == M && M == N && N == K) {
+    do_avx (lda, M, N, K, A, B, C);
+  } else {
+    do_simple (lda, M, N, K, A, B, C);
+  }
+  //printf("exiting a block\n"); 
+}
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */
 void square_dgemm (/*int iii,*/int lda, double* A, double* B, double* C)
 {
-  printf("Do you see me now at %d\t \n",lda);
-  //This is to print out a small n*n C matrix (before calculation)
-  /*if (lda == 8) {
-    for (int i = 0; i < lda; ++i) {
-      for (int k = 0; k < lda; ++k) {
-        printf("%g\t ",C[i+lda*k]);
-      }
-      printf("\n");
-    }
-  }*/
-
+  //printf("Do you see me now at %d\t \n",lda);
   //Block size in 1D for L2 cache - BLOCK2 - for L1 cache - BLOCK 1 - 
-  int BLOCK1 = 64;
-  int BLOCK2 = 128;
-  
-    
+  int BLOCK1 = 32;
+  int BLOCK2 = 64;
   //Proposed blocking method for 2 levels of memory - L1 and L2
   for (int x = 0; x < lda; x += BLOCK2) {
     int lim_k = x + min (BLOCK2,lda-x);
@@ -118,16 +110,4 @@ void square_dgemm (/*int iii,*/int lda, double* A, double* B, double* C)
       }
     }
   }
- 
-
-  //This is to print out a small n*n C matrix (after calculation)
-  /*if (lda == 96) {
-    for (int i = 0; i < lda; ++i) {
-      for (int k = 0; k < lda; ++k) { 
-	printf("%g\t ",C[i+lda*k]);
-      }
-      printf("\n");
-    }
-  }*/
-  //printf("\n");
 }
